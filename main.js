80fdf652dae0cf82edb986b9ce1fa991d6a59432
Aayushi Tawari <tawari_aayushi.it@ghrce.raisoni.net>
2021-10-03 11:03:30 +0530

    btn

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let products=document.getElementsByClassName("product");
-let product=products[0];
-let grid=document.getElementById("grid-holder");
-for(let index=0; index<10;index++){
-    grid.aapendChild(product);
-}
-
+//let products=document.getElementsByClassName("product");
+//let product=products[0];
+//let grid=document.getElementById("grid-holder");
+const button=document.getElementsByClassName("btn btn-primary py-1 px-4 ");
+console.log(buttons);
+let cartItems=0;
+for(let button of buttons){
+button.addEventListener(
+        'click',
+     myCkickEvent=>{
+        cartItems +=1;
+        console.log(cartItems);
+     }
+    );
+    }
